0ee90cdb652e9d10f5a560cd95e00dee1224b495
timkanda <tmthkanda@gmail.com>
2022-12-04 13:44:41 -0800

    fixing errors

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,5 @@ def print_last_digit(number):
     for ld in number:
         ld = ord(i)
     ld = number % 10 if number >= 0 else number * (-1) % 10
-    if number < 0:
-        ld = -int(ld)
     print(number, end="")
     print(ld)
