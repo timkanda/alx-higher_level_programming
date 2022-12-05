c3b09d94b993c14a8e892a2d960d63e1c7a8bbaa
timkanda <tmthkanda@gmail.com>
2022-12-05 13:15:38 -0800

    removing all char from string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    new_string = list(my_string)
+    index_count = 0
+    for idx in new_string:
+        if idx == 'c' or idx == 'C':
+            new_string[index_count] = ""
+            index_count += 1
+            return "".join(new_string)
