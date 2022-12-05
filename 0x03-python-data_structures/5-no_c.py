f6dd15ad390c35ebbcd88c51e1eba9255851a595
timkanda <tmthkanda@gmail.com>
2022-12-05 13:45:06 -0800

    removing characters

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     new_string = list(my_string)
-    index_count = 0
     for idx in new_string:
-        if idx != 'c' or idx != 'C':
-            new_string[index_count] = ""
-            index_count += 1
+        if idx != 'c' and idx != 'C':
+            new_string += idx
             return new_string
