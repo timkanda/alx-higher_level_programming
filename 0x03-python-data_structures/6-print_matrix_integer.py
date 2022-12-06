deeeddd71218a7e9c1f0f9c8d16f270b75849335
timkanda <tmthkanda@gmail.com>
2022-12-05 23:54:14 -0800

    fixing matrix errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print(i)
+    if not matrix:
+        print()
+    else:
+        for row in range(len(matrix)):
+            for i in range(len(matrix[row])):
+                if i != len(matrix[row]) - 1:
+                    endspace = ' '
+                else:
+                    endspace = ''
+                print("{:d}" .format(matrix[row][i]), end=endspace)
+                print()
