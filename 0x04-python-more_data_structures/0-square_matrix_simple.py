7b5c363fa28b29d44873492e6fe005d4ab8cd76d
timkanda <tmthkanda@gmail.com>
2022-12-09 12:23:30 -0800

    computing squares in matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix[]
-    for n in new_matrix[row]:
-        print(new_matrix)
+    for n in new_matrix:
+       return [[n ** 2 for n in row] for row in new_matrix]
