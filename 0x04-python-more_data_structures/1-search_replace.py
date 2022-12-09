b0699d1d72280ad7a9912008c45c5c079172b8c6
timkanda <tmthkanda@gmail.com>
2022-12-09 13:46:20 -0800

    replacing elements

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@ def search_replace(my_list, search, replace):
     for idx, item in enumerate(my_list):
         if item == search:
             my_list[idx] = replace
-        print(my_list)
+        return my_list
