f7752bfec97af261d93bb41f167823441767e4a8
timkanda <tmthkanda@gmail.com>
2022-12-12 01:58:14 -0800

    fixing errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
         print("{:d} arguments." .format(argv_count))
     elif argv_count is 1:
         print("{:d} argument:" .format(argv_count))
-        print("{:d} {:s}" .format(index, sys.argv[1]))
+        print("{:d}: {:s}" .format(index, sys.argv[1]))
     else:
         print("{:d} arguments:" .format(argv_count))
         while index <= argv_count:
-            print("{:d} {:s}" .format(index, sys.argv[index]))
+            print("{:d}: {:s}" .format(index, sys.argv[index]))
             index += 1
