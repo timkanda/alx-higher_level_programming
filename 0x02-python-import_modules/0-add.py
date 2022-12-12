42b6b0110a6c84f9b0ed4a18bea66c5cee3d9850
timkanda <tmthkanda@gmail.com>
2022-12-12 00:28:10 -0800

    fixing errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def add(a, b):
-    if __name__ == "__main__":
-        import add_0
-        a = 1
-        b = 2
-        print("{:d} + {:d} = {:d}" .format(a, b, add(a + b)))
+from add_0 import add
+a = 1
+b = 2
+print("{:d} + {:d} = {:d}" .format(a, b, add(a + b)))
